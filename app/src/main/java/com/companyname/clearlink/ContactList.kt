2e41368c1a87ba8925c972aa09ee2edbaa8d98e2
jiyeon <cn0714@naver.com>
2023-09-04 19:09:09 +0900

    [DESIGN] : MyPage Layout

--- app/src/main/java/com/companyname/clearlink/ContactList.kt
+++ app/src/main/java/com/companyname/clearlink/ContactList.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ContactList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.contactlist_activity)
+        setContentView(R.layout.mypage_activity)
     }
 }
